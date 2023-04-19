cdb0baad2b0b34e9f1f044ee2db33e0cec96769b
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2023-04-19 08:21:35 +0200

    Initial Commit
    Initial upload with all project files. Working as expected.

---
+++ README.md
@@ -0,0 +1,16 @@
+# OMSI-LogHelper
+
+This tool is an attempt to make the OMSI log file more understandable and accessible. 
+
+Often the log file is confusing, and important information is hidden between unimportant information. 
+
+With this tool, the focus should be put on the really critical things. That's why all errors are right at the beginning, so that you can start fixing them immediately.
+
+# Technical Details 
+* App Framework: Electron
+* contextIsolation from Electron is enabled
+* build in security features used
+* Fully written with JavaScript and HTML
+
+# Known Problems
+* Currently there is no UI
---
+++ preload.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const { ipcRenderer, contextBridge } = require('electron')
+
+// All the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+
+window.addEventListener('DOMContentLoaded', () => {
+    const replaceText = (selector, text) => {
+      const element = document.getElementById(selector)
+      if (element) element.innerText = text
+    }
+  
+    for (const type of ['chrome', 'node', 'electron']) {
+      replaceText(`${type}-version`, process.versions[type])
+    }
+  })
+
+  contextBridge.exposeInMainWorld('FileReaderAPI', {
+    readFile: async (filePath) => {
+      return fs.readFileSync(filePath, 'utf8')
+    }
+  })
+
+contextBridge.exposeInMainWorld('FileDialogAPI', {
+  openFileDialog: async () => await ipcRenderer.invoke('logfile-dialog')
+})
